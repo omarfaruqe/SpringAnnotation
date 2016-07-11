402178d2f682b201b91747a1481651f48e2c052b
Omar Faruqe <faruqe.cse@gmail.com>
2016-07-11 11:11:18 -0400

    Required Annotation

---
+++ src/com/mc/MainApp.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author faruqe
+ */
+public class MainApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+
+      Student student = (Student) context.getBean("student");
+
+      System.out.println("Name : " + student.getName() );
+      System.out.println("Age : " + student.getAge() );
+    }
+    
+}
---
+++ src/com/mc/Student.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mc;
+
+import org.springframework.beans.factory.annotation.Required;
+
+/**
+ *
+ * @author faruqe
+ */
+public class Student {
+
+    private Integer age;
+    private String name;
+
+    @Required
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    @Required
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
